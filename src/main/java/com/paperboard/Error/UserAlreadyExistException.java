16456c03ada63ea25068e777761ac81f722afffa
brieuc <brieuc.loussouarn@orange.fr>
2019-10-01 15:42:00 +0200

    Add test and correct http routes

--- src/main/java/com/paperboard/Error/UserAlreadyExistException.java
+++ src/main/java/com/paperboard/Error/UserAlreadyExistException.java
@@ -1,9 +1,13 @@
 package com.paperboard.Error;
 
 import com.paperboard.server.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "User already exists")
 public class UserAlreadyExistException extends RuntimeException {
-    public UserAlreadyExistException(User user) {
-        super(user.toString() + " already exists");
+
+    public UserAlreadyExistException(final User user) {
+        super("User : " + user.toString() + " already exists");
     }
 }
--- src/main/java/com/paperboard/server/PaperBoard.java
+++ src/main/java/com/paperboard/server/PaperBoard.java
@@ -2,21 +2,89 @@ package com.paperboard.server;
 
 import com.paperboard.drawings.Drawing;
 
+import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class PaperBoard {
-  private String title;
-  private String backgroundColor;
-  private java.util.Set<User> drawers = new ConcurrentSkipListSet<User>();
-  private java.util.concurrent.CopyOnWriteArrayList<com.paperboard.drawings.Drawing> drawings =
-      new CopyOnWriteArrayList<Drawing>();
-  private byte[] backgroundImage;
-  private String id;
+    private static AtomicLong idCounter = new AtomicLong(0);
+    final private String id;
+    final private String title;
+    private String backgroundColor;
+    private java.util.Set<User> drawers = new ConcurrentSkipListSet<User>();
+    private java.util.concurrent.CopyOnWriteArrayList<com.paperboard.drawings.Drawing> drawings =
+            new CopyOnWriteArrayList<Drawing>();
+    private byte[] backgroundImage;
 
-  public PaperBoard(String title) {}
+    public PaperBoard(final String title) {
+        this.id = String.valueOf(idCounter.getAndIncrement());
+        this.title = title;
+    }
 
-  public PaperBoard(String title, String backgroundColor) {}
+    public PaperBoard(final String title, final String backgroundColor) {
+        this.id = String.valueOf(idCounter.getAndIncrement());
+        this.title = title;
+        this.setBackgroundColor(backgroundColor);
+    }
 
-  public PaperBoard(String title, byte[] image) {}
+    public PaperBoard(final String title, final byte[] image) {
+        this.id = String.valueOf(idCounter.getAndIncrement());
+        this.title = title;
+        this.setBackgroundImage(image);
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(final String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+
+    public Set<User> getDrawers() {
+        return drawers;
+    }
+
+    public CopyOnWriteArrayList<Drawing> getDrawings() {
+        return drawings;
+    }
+
+
+    public byte[] getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    public void setBackgroundImage(final byte[] backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.title;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if ((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+        final PaperBoard paperBoard = (PaperBoard) obj;
+        return this.getTitle().equals(paperBoard.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getTitle().length();
+    }
 }
