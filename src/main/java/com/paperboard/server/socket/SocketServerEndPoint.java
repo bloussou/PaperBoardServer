e5b2656a499cdadb43f0c7ad63dd50c7fd98e8db
Ludo <ludovic.descateaux@student.ecp.fr>
2019-11-02 16:58:07 +0100

    Implemented Join Board, Leave Board and Chat Message

--- src/main/java/com/paperboard/server/socket/SocketServerEndPoint.java
+++ src/main/java/com/paperboard/server/socket/SocketServerEndPoint.java
@@ -1,8 +1,15 @@
 package com.paperboard.server.socket;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @ServerEndpoint(
@@ -11,12 +18,14 @@ import java.util.logging.Logger;
         decoders = MessageDecoder.class,
         configurator = WebSocketServerConfigurator.class)
 public class SocketServerEndPoint {
+    private static HashSet<Session> sessions = new HashSet<Session>();
     private Logger log = Logger.getLogger(getClass().getName());
 
     @OnOpen
     public void open(final Session session, @PathParam("board") final String board) {
         log.info("New user connected to board [" + board + "] !! userId:" + session.getId());
         session.getUserProperties().put("board", board);
+        this.sessions.add(session);
     }
 
     /**
@@ -34,46 +43,36 @@ public class SocketServerEndPoint {
 
         switch (MessageType.getEnum(message.getType())) {
             case JOIN_BOARD:
-                System.out.println("Should call handleJoinBoardMsg(message, session)");
+                System.out.println("Should call handleJoinBoardMsg");
+                this.handleJoinBoard(session, message.getFrom());
                 break;
             case LEAVE_BOARD:
-                System.out.println("Should call handleLeaveBoard(message, session)");
+                System.out.println("Should call handleLeaveBoard");
+                this.handleLeaveBoard(session, message.getFrom());
                 break;
             case CREATE_OBJECT:
-                System.out.println("Should call handleCreateObject(message, session)");
+                System.out.println("Should call handleCreateObject");
                 break;
             case EDIT_OBJECT:
-                System.out.println("Should call handleEditObject(message, session)");
+                System.out.println("Should call handleEditObject");
                 break;
             case LOCK_OBJECT:
-                System.out.println("Should call handleLockObject(message, session)");
+                System.out.println("Should call handleLockObject");
                 break;
             case UNLOCK_OBJECT:
-                System.out.println("Should call handleUnlockObject(message, session)");
+                System.out.println("Should call handleUnlockObject");
                 break;
             case ASK_DELETION:
-                System.out.println("Should call handleAskDeletion(message, session)");
+                System.out.println("Should call handleAskDeletion");
                 break;
             case CHAT_MESSAGE:
-                System.out.println("Should call handleChatMessage(message, session)");
+                System.out.println("Should call handleChatMessage");
+                this.handleChatMessage(message);
                 break;
             default:
                 System.out.println("Message Type Not Recognized !!");
                 break;
         }
-
-        /* A garder pour l'inspi pour un broadcast
-        try {
-            for (Session s : session.getOpenSessions()) {
-                if (s.isOpen()
-                        && board.equals(s.getUserProperties().get("board"))) {
-                    s.getBasicRemote().sendObject(chatMessage);
-                }
-            }
-        } catch (IOException | EncodeException e) {
-            logger.log(Level.WARNING, "onMessage failed", e);
-        }
-        */
     }
 
     @OnClose
@@ -81,12 +80,83 @@ public class SocketServerEndPoint {
         log.info(String.format("Session %s closed because of %s", session.getId(), closeReason));
     }
 
-    // TODO
-    public void sendMessageToUser(final String username) {
-
+    public void sendMessageToUser(final String username, final Message message) {
+        try {
+            for (final Session s : this.sessions) {
+                if (s.isOpen()
+                        && username.equals(s.getUserProperties().get("username"))) {
+                    s.getBasicRemote().sendObject(message);
+                    break;
+                }
+            }
+        } catch (final IOException | EncodeException e) {
+            log.log(Level.WARNING, "sendMessageToUser [" + username + "] failed", e);
+        }
     }
 
-    // TODO
-    public void sendMessageToBoard(final String board) {
+    public void sendMessageToBoard(final String board, final Message msg) {
+        try {
+            for (final Session s : this.sessions) {
+                if (s.isOpen()
+                        && board.equals(s.getUserProperties().get("board"))) {
+                    s.getBasicRemote().sendObject(msg);
+                }
+            }
+        } catch (final IOException | EncodeException e) {
+            log.log(Level.WARNING, "sendMessageToBoard [" + board + "] failed", e);
+        }
+    }
+
+    public void handleJoinBoard(final Session session, final String newUser) {
+        session.getUserProperties().put("username", newUser);
+        final String board = (String) session.getUserProperties().get("board");
+
+        final JsonBuilderFactory factory = Json.createBuilderFactory(null);
+        final JsonArrayBuilder users = factory.createArrayBuilder();
+        for (final Session s : this.sessions) {
+            final String username = (String) s.getUserProperties().get("username");
+            if (s.isOpen() && username != null) {
+                users.add(username);
+            }
+        }
+
+        final JsonObject payload = Json.createBuilderFactory(null).createObjectBuilder()
+                .add("username", newUser)
+                .add("userlist", users)
+                .build();
+
+        final Message answer = new Message(MessageType.DRAWER_CONNECTED.str, "server", "all-board", payload);
+        this.sendMessageToBoard(board, answer);
+    }
+
+    public void handleLeaveBoard(final Session session, final String oldUser) {
+        session.getUserProperties().put("username", oldUser);
+        final String board = (String) session.getUserProperties().get("board");
+
+        final JsonBuilderFactory factory = Json.createBuilderFactory(null);
+        final JsonArrayBuilder users = factory.createArrayBuilder();
+        for (final Session s : this.sessions) {
+            final String username = (String) s.getUserProperties().get("username");
+            if (s.isOpen() && username != null && username != oldUser) {
+                users.add(username);
+            }
+        }
+
+        final JsonObject payload = Json.createBuilderFactory(null).createObjectBuilder()
+                .add("username", oldUser)
+                .add("userlist", users)
+                .build();
+
+        try {
+            session.close();
+        } catch (final IOException ex) {
+            System.err.println("Could not close session properly with user " + oldUser);
+        }
+        final Message answer = new Message(MessageType.DRAWER_DISCONNECTED.str, "server", "all-board", payload);
+        this.sendMessageToBoard(board, answer);
+    }
+
+    public void handleChatMessage(final Message msg) {
+        this.sendMessageToUser(msg.getTo(), msg);
     }
 }
